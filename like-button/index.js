cccc9c7a36af168622bb0acbd3f64c8018c92f83
Andrii Kycha <andrii.kycha@brivity.com>
2025-12-15 20:18:04 -0700

    WIP: like button

--- like-button/index.js
+++ like-button/index.js
@@ -1,7 +1,11 @@
-const NUM_PARTICLES = 10;
-const LIKE_BTN_SIZE = 52;
-const ICON_SIZE = 48;
-const PARTICLE_ANIMATION_DURATION = 1000;
+// todo akicha: these should be configurable for the demo
+const NUM_PARTICLES = 50;
+const LIKE_BTN_SIZE = 104;
+const RIPPLE_SIZE = 104;
+const PARTICLE_ANIMATION_DURATION = 500;
+const TWINKLE_ANIMATION_DURATION = 300;
+const MAX_PARTICLE_ANIMATION_DEVIATION = 250;
+const MAX_TWINKLE_ANIMATION_DEVIATION = 100;
 
 function main() {
   const likeBtn = document.getElementById("like");
@@ -16,25 +20,47 @@ function main() {
     const particles = [];
 
     for (let i = 0; i < NUM_PARTICLES; i++) {
-      const radius = ICON_SIZE / 2;
+      const radius = RIPPLE_SIZE / 2;
 
       const angle = Math.random() * 2 * Math.PI;
-      const tx = radius * Math.cos(angle);
-      const ty = radius * Math.sin(angle);
+      const deviation = (Math.random() > 0.5 ? 1 : -1) * 10 * Math.random();
+      const tx = (radius + deviation) * Math.cos(angle);
+      const ty = (radius + deviation) * Math.sin(angle);
+
+      const animationDurationDeviation =
+        (Math.random() > 0.5 ? 1 : -1) *
+        Math.random() *
+        MAX_PARTICLE_ANIMATION_DEVIATION;
 
       const particle = document.createElement("span");
       particle.classList.add("like-particle");
       particle.style.setProperty("--tx", `${tx}px`);
       particle.style.setProperty("--ty", `${ty}px`);
       particle.style.setProperty("--hue", Math.random() * 360);
+
+      particle.style.setProperty(
+        "--animation-duration",
+        `${PARTICLE_ANIMATION_DURATION + animationDurationDeviation}ms`
+      );
+
+      const twinkleAnimationDurationDeviation =
+        (Math.random() > 0.5 ? 1 : -1) *
+        Math.random() *
+        MAX_TWINKLE_ANIMATION_DEVIATION;
+
+      particle.style.setProperty(
+        "--twinkle-animation-duration",
+        `${TWINKLE_ANIMATION_DURATION + twinkleAnimationDurationDeviation}ms`
+      );
+
       likeBtn.appendChild(particle);
       particles.push(particle);
     }
 
     // todo akicha: cover an edge case where user clicks multiple times quickly
     setTimeout(() => {
-      particles.forEach((particle) => particle.remove());
-    }, PARTICLE_ANIMATION_DURATION);
+      // particles.forEach((particle) => particle.remove());
+    }, OVERALL_ANIMATION_DURATION);
   });
 }
 
